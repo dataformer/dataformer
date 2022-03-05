14c8d673e2bbf594eb2d451dbd7682f77deb8c1d
Adam Dejl <adam.dejl18@imperial.ac.uk>
2022-03-05 17:26:52 -0500

    Use Prettier formatter for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [serverMessage, setServerMessage] = useState("...")
+  const [serverMessage, setServerMessage] = useState("...");
 
   useEffect(() => {
-    fetch('/hello?name=Frontend')
-      .then(response => response.text())
-      .then(msg => setServerMessage(msg))
-  })
+    fetch("/hello?name=Frontend")
+      .then((response) => response.text())
+      .then((msg) => setServerMessage(msg));
+  });
 
   return (
     <div className="App">
@@ -17,9 +17,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to Dataformer!</h2>
       </div>
-      <p className="App-intro">
-        Server says: {serverMessage}
-      </p>
+      <p className="App-intro">Server says: {serverMessage}</p>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
