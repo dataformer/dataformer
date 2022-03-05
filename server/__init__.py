ddee8ab9a3fcd9ccc295ad42dcee2a7c86a3fa0a
Adam Dejl <adam.dejl18@imperial.ac.uk>
2022-03-05 15:01:23 -0500

    Use black formatter

--- server/__init__.py
+++ server/__init__.py
@@ -1,19 +1,21 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 from flask import Flask, escape, request, send_from_directory
 from flask_restful import Api
 
+
 def init_api(app):
     api = Api(app)
 
+
 def create_app():
-    app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+    app = Flask(__name__, static_folder="../frontend/build", static_url_path="")
 
     init_api(app)
 
     @app.route("/")
     def index():
-        return send_from_directory(app.static_folder, 'index.html')
+        return send_from_directory(app.static_folder, "index.html")
 
     @app.route("/status")
     def status():
@@ -23,5 +25,5 @@ def create_app():
     def echo():
         name = request.args.get("name", "World")
         return f"Hello {escape(name)}!"
-    
+
     return app
