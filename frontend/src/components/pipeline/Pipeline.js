601a5a3f73c2865593cb62656fcbdf00c7161058
Evan Rubel <erubel@mit.edu>
2022-04-17 21:01:49 -0400

    Centered pipeline components

--- frontend/src/components/pipeline/Pipeline.js
+++ frontend/src/components/pipeline/Pipeline.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { Card, CardActionArea, CardActions, CardContent } from "@mui/material";
 import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 
 function Pipeline() {
   // const [input, setInput] = useState("");
@@ -15,41 +20,59 @@ function Pipeline() {
   const buttons = ["Edit", "Disable", "Export"];
 
   return (
-    <Box sx={{maxWidth: "360px"}} justifyContent="center">
-      <List
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <List>
         {commands.map((command) => (
-          <Card sx={{ paddingTop: 5, justifyContent: "center" }}>
-            <CardActionArea>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {command}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  This is an example command.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions sx={{ justifyContent: "center" }}>
-              {buttons.map((button) => (
-                <Button size="small" color="primary">
-                  {button}
-                </Button>
-              ))}
-            </CardActions>
-          </Card>
+          <ListItem>
+            <Card variant="outlined" sx={{ width: 500 }}>
+              <CardActionArea>
+                <CardContent alignItems="center">
+                  <Typography gutterBottom variant="h5" component="div" align="center">
+                    {command}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" align="center">
+                    This is an example command.
+                  </Typography>
+                  <TextField
+                    id="outlined-basic"
+                    label="Your Regex"
+                    variant="outlined"
+                  />
+                </CardContent>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Type"
+                  >
+                    <MenuItem value={0}>Literal String</MenuItem>
+                    <MenuItem value={1}>Regex</MenuItem>
+                  </Select>
+                </FormControl>
+              </CardActionArea>
+              <CardActions sx={{ justifyContent: "center" }}>
+                {buttons.map((button) => (
+                  <Button size="small" color="primary">
+                    {button}
+                  </Button>
+                ))}
+              </CardActions>
+            </Card>
+          </ListItem>
         ))}
-        <br />
+        {/* <br /> */}
         <Button variant="contained" onClick={() => console.log("Executing...")}>
           Pipeline Execute
         </Button>
       </List>
-    </Box>
+    </Grid>
   );
 }
 
