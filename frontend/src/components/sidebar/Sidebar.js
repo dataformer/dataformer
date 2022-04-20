7150adcdb87f9a3727af8f1fe406be509a1f3036
Adam Dejl <adam.dejl18@imperial.ac.uk>
2022-04-20 16:01:53 -0400

    Implement sidebar resizing

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,7 +1,49 @@
 import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import SidebarGroup from "./SidebarGroup";
+import FindReplaceIcon from "@mui/icons-material/FindReplace";
+import useResize from "../../utils/resizing";
 
-function SideBar() {
-  return <div className="Sidebar"></div>;
+function Sidebar(props) {
+  const [width, enableResize] = useResize("max(20%, 250px)", 250);
+
+  const commandGroups = props.commandGroups.map((cg) => (
+    <SidebarGroup
+      name={cg.name}
+      icon={cg.icon}
+      commands={cg.commands}
+      key={cg.name}
+    />
+  ));
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box sx={{ width: width, flexShrink: 0 }}>
+        <Drawer
+          variant="permanent"
+          sx={{
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: width,
+            },
+          }}
+          open
+        >
+          <List>{commandGroups}</List>
+        </Drawer>
+      </Box>
+      <Box
+        sx={{
+          height: "100vh",
+          width: "3px",
+          cursor: "col-resize",
+        }}
+        onMouseDown={enableResize}
+      />
+    </Box>
+  );
 }
 
 export default Sidebar;
