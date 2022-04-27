32e4721d65de0e834ce52200b12154350bb588a8
Adam Dejl <adam.dejl18@imperial.ac.uk>
2022-04-27 16:03:53 -0400

    Run executed commands in shell mode on server side

--- server/api/execute.py
+++ server/api/execute.py
@@ -7,10 +7,8 @@ from flask_restful import Resource
 class ExecuteResource(Resource):
     def post(self, command):
         command_output = subprocess.run(
-            command.split(" "), stdout=subprocess.PIPE
+            command, stdout=subprocess.PIPE, shell=True
         ).stdout.decode("utf-8")
-        output = "TODO: Execute \'" + command + "\' in backend correctly"
-        output += "\nCurrent output: \'" + command_output + "\'"
-        response = make_response(output, 200)
+        response = make_response(command_output, 200)
         response.mimetype = "text/plain"
         return response
